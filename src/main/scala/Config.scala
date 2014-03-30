d50834f49c16871a256cdefda9f6113796bf6a93
softprops <d.tangren@gmail.com>
2014-03-30 18:14:43 -0400

    make config more query friendly

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -1,18 +1,29 @@
 package harp
 
 case class Config(sections: Seq[Section]) {
-  lazy val defaults = sections find(_.name == "defaults")
-  lazy val global = sections find(_.name == "global")
+  lazy val defaults = sections find(_.name == Parse.Defaults)
+
+  lazy val global = sections find(_.name == Parse.Global)
+
+  lazy val frontends: Iterable[Section.Frontend] =
+    (List.empty[Section.Frontend] /: sections) {
+      case (ends, fe: Section.Frontend) => fe :: ends
+      case (ends, _) => ends
+    }
+
+  lazy val backends: Iterable[Section.Backend] =
+    (List.empty[Section.Backend] /: sections) {
+      case (ends, be: Section.Backend) => be :: ends
+      case (ends, _) => ends
+    }
 
   def frontend(name: String) =
-    sections find {
-      case Section.Frontend(`name`, _, _) => true
-      case _ => false
+    frontends find {
+      case Section.Frontend(fe, _, _) => fe == name
     }
 
   def backend(name: String) =
-    sections find {
-      case Section.Backend(`name`, _, _) => true
-      case _ => false
+    backends find {
+      case Section.Backend(be, _, _) => be == name
     }
 }
